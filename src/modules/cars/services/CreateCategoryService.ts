4d9f25fd227ca95a7ff2ec76c3e448ca212ddd7d
Izaias Lima <53953937+IzaiasMorais@users.noreply.github.com>
2022-10-01 19:28:39 -0300

    chore: Complete modeling entities

---
+++ src/modules/cars/services/CreateCategoryService.ts
@@ -0,0 +1,22 @@
+import { ICategoriesRepository } from "../repositories/ICategoriesRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+class CreateCategoryService {
+  constructor(private categoriesRepository: ICategoriesRepository) {}
+
+  execute({ name, description }: IRequest): void {
+    const CategoryAlreadyExists = this.categoriesRepository.findByName(name);
+
+    if (CategoryAlreadyExists) {
+      throw new Error("Category already exists!");
+    }
+
+    this.categoriesRepository.create({ name, description });
+  }
+}
+
+export { CreateCategoryService };
